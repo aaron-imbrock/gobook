dff3911cf23f5bc4de33ae7b48924072c3c75368
Aaron Imbrock <aimbrock@gmail.com>
2020-05-13 07:39:20 -0700

    Modified echo program to also print name of command that invoked it

--- ch1/echo11/echo11.go
+++ ch1/echo11/echo11.go
@@ -1,10 +1,7 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 8.
-
-// Echo11 Exercise 1.1: Modify the echo program to also print os.Args[0],
+// Echo11 
+// Exercise 1.1: Modify the echo program to also print os.Args[0],
 // the name of the command that invoked it.
+// See page 8.
 
 package main
 
@@ -14,9 +11,7 @@ import (
 	"strings"
 )
 
-//!+
 func main() {
 	fmt.Println(strings.Join(os.Args, " "))
 }
 
-//!-
